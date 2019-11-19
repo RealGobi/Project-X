ccb8624edc713cfae48ae8dab4f13325f2fb8164
Åsa Palholmen <asa@palholmen.se>
2019-11-19 13:35:09 +0100

    added reacthtml

--- src/pages/Choose-second.js
+++ src/pages/Choose-second.js
@@ -2,15 +2,41 @@ import React from 'react';
 import Link from 'next/link';
 
 import Page from '../Components/Page/Page';
+import Header from '../Components/Header/Header';
+import Button from '../Components/Button/Button';
 
 export default function ChooseSecond() {
+  const bg1 = {
+    background: '#B7DDE0',
+  };
+  const bg2 = {
+    background: '#FEE19F',
+  };
+  const bg3 = {
+    background: '#FFC79B',
+  };
+
   return (
     <div>
       <Page>
-        [Choose Second]
-        <Link href="/Recipt-list">
-          <a>Recipt-list</a>
-        </Link>
+        <Header headLine="Välj Andra" />
+        <div className="select-button">
+          <Button buttonText="Ris" bg={bg1} />
+          <Button buttonText="Pasta" bg={bg2} />
+          <Button buttonText="Potatis" bg={bg3} />
+        </div>
+        <div className="next-page">
+          <Link href="/Choose-second">
+            <a>
+              <Button buttonText="Back" bg={bg1} />
+            </a>
+          </Link>
+          <Link href="/Recipt-list">
+            <a>
+              <Button buttonText="Välj Andra" bg={bg1} />
+            </a>
+          </Link>
+        </div>
       </Page>
     </div>
   );
--- src/pages/Recipt-list.js
+++ src/pages/Recipt-list.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Link from 'next/link';
 
 import Page from '../Components/Page/Page';
+import Header from '../Components/Header/Header';
+import Button from '../Components/Button/Button';
 
 export default function ReciptList() {
   return (
     <div>
       <Page>
-        [Recipt List]
+        <Header headLine="Receptlista" />
         <Link href="/Recipt-page">
           <a>Recipt-page</a>
         </Link>
@@ -16,6 +18,11 @@ export default function ReciptList() {
         </Link>
         <Link href="/Recipt-page">
           <a>Recipt-page</a>
+        </Link>
+        <Link href="/Choose-second">
+          <a>
+            <Button buttonText="Tillbaka" />
+          </a>
         </Link>
       </Page>
     </div>
