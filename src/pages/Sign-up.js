788adedfe22eb64004fc0a2e99c6cc435a5ded08
Åsa Palholmen <asa@palholmen.se>
2019-12-02 13:18:04 +0100

    Fixed styling on index and Sign-up

--- src/pages/Sign-up.js
+++ src/pages/Sign-up.js
@@ -6,24 +6,43 @@ import Page from '../Components/Page/Page';
 import Header from '../Components/Header/Header';
 import Button from '../Components/Button/Button';
 
+import Background from '../images/monika-grabkowska-white.jpg';
+
 export default function SignUp() {
   const headLine = 'Skapa Konto';
+  const styleback = {
+    backgroundImage: `url(${Background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
 
   return (
     <div>
-      <Header headLine={headLine} /> 
-      <Page>               
-        <div className="select-button">
-          <Button buttonText="Vegan" color="mint" />
-          <Button buttonText="Vegetarian" color="yellow" />
-          <Button buttonText="Allätare" color="persica" />
-        </div>
-        <div className="next-page">
-          <Link href="/Landing-page">
-            <a>
-              <Button buttonText={headLine} color="mint" />
-            </a>
-          </Link>
+      <Header headLine={headLine} />
+      <Page>
+        <div className="sign-up" style={styleback}>
+          <div className="input-container">
+            <span>Namn:</span> <input type="text" />
+          </div>
+          <div className="input-container">
+            <span>E-post:</span> <input type="email" />
+          </div>
+          <div className="input-container">
+            <span>Lösenord:</span> <input type="password" />
+          </div>
+          <div className="select-button">
+            <Button buttonText="Vegan" color="mint" />
+            <Button buttonText="Vegetarian" color="yellow" />
+            <Button buttonText="Allätare" color="persica" />
+          </div>
+          <div className="next-page">
+            <Link href="/Landing-page">
+              <a>
+                <Button buttonText={headLine} color="mint" />
+              </a>
+            </Link>
+          </div>
         </div>
       </Page>
     </div>
