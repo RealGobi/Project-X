c3470c68f3245de212f9372ed74ff88ee447774c
Jimmy Svensson <jimmy888swe@gmail.com>
2019-12-09 00:06:45 +0100

    chooseFirst works with data stp 1

--- src/pages/Choose-first.js
+++ src/pages/Choose-first.js
@@ -6,20 +6,25 @@ import Header from '../Components/Header/Header';
 import Button from '../Components/Button/Button';
 
 export default function ChooseFirst(props) {
-
-  const category = [];
-  const sortCategory = () => {
+  // collect all categorys to one array (and color)
+  let category = [];
+  const collectCategory = () => {
     props.recipe.map(cat => cat.category1.map(tac => category.push(tac)));
     console.log(category);
   };
-  sortCategory();
+  collectCategory();
+  // remove duplicates
+  const myData = category;
+  category = Array.from(new Set(myData.map(JSON.stringify))).map(JSON.parse);
+  console.log(category);
+
   return (
     <div>
       <Header headLine="Välj Första" />
       <Page>
         <span className="centerGrid">
           <div className="choose-button">
-            { props.recipe.map( color => category.map(c => <Button key={c} buttonText={c} color={color.color} buttonType="square" />))}
+            { category.map((cat1, idx) => <Button key={idx} buttonText={cat1.value} buttonType="square" color={cat1.color} />) }
           </div>
         </span>
         <div className="next-page">
