63905f1f97f31707aad8cf0b969122e4e55f94ad
asayehla <asa@palholmen.se>
2019-12-11 17:44:14 +0100

    dynamic number of ingridients, almost there

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
 import Button from '../Components/Button/Button';
 
 export default function Admin() {
+  let numberOfIngridients = 0;
+
+  const handleNumberIngridients = (e) => {
+    numberOfIngridients = e.target.value;
+    console.log(numberOfIngridients);
+  };
+
   return (
     <div className="admin">
       <h1>Admin</h1>
@@ -11,6 +17,21 @@ export default function Admin() {
       </div>
       <div className="admin-addrecept">
         <h2>Nytt Recept</h2>
+        <h4>Antal Ingredienser:</h4>
+        <form action="submit">
+          <select className="portioner" onChange={handleNumberIngridients}>
+            <option value="1">1 ingredienser</option>
+            <option value="2">2 ingredienser</option>
+            <option value="3">3 ingredienser</option>
+            <option value="4">4 ingredienser</option>
+            <option value="5">5 ingredienser</option>
+            <option value="6">6 ingredienser</option>
+            <option value="7">7 ingredienser</option>
+            <option value="8">8 ingredienser</option>
+            <option value="9">9 ingredienser</option>
+            <option value="10">10 ingredienser</option>
+          </select>
+        </form>
         <form action="send">
           <label>Namn: <input type="text" /></label>
           <label>Beskrivning: <input type="text" /></label>
@@ -19,17 +40,27 @@ export default function Admin() {
           <label>Bildlänk:<input type="text" /></label>
           <label>Tid: <input type="text" /></label>
 
-          <p>Ingrediens 1:</p>
-          <label>Antal:<input type="text" /></label>
-          <label>Enhet:<input type="text" /></label>
-          <label>Vara:<input type="text" /></label>
+          {
+            numberOfIngridients ? (
+              [...Array(numberOfIngridients)].map((e, i) => (
+                <div key={i}>
+                  <p>Ingrediens 1:</p>
+                  <label>Antal:<input type="text" /></label>
+                  <label>Enhet:<input type="text" /></label>
+                  <label>Vara:<input type="text" /></label>
+                </div>
+              ))
+            ) : (
+              <p>välj antal ingredienser:</p>
+            )
+          }
           <hr />
 
           <label>Instruktioner 1:<input type="text" /></label>
           <label>Instruktioner 2:<input type="text" /></label>
           <label>Allergy:<input type="text" /></label>
           <label>Rating:<input type="text" /></label>
-        </form><Button type="submit"> Lägg till </Button>
+        </form><Button type="submit" buttonText="Lägg till"> </Button>
 
       </div>
     </div>
