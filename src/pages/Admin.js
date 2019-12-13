1e8e9ae5e35af0d54ad3a37265315204b1aa8e9d
Jimmy Svensson <jimmy888swe@gmail.com>
2019-12-13 09:31:06 +0100

    removed code

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -4,16 +4,9 @@ import Button from '../Components/Button/Button';
 import useFetch from '../Hooks/useFetch';
 
 export default function Admin(props) {
-  let numberOfIngridients = 3;
-  console.log(props);
-
   const [newRecipe, setNewRecipe] = useState({});
   const [loading, setLoading] = useState(false);
 
-  const handleNumberIngridients = (e) => {
-    numberOfIngridients = (e.target.value);
-    console.log(numberOfIngridients);
-  };
   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlczB0QHRlc3Quc2UiLCJ1c2VySWQiOiI1ZGU3YWZhODU2MjA1YzBkMjFhZDg2YjQiLCJpYXQiOjE1NzYxNTM5MTgsImV4cCI6MTU3NjE1NzUxOH0.qAfKpwuxaf10z-yibeeUIojI6fK42nVgErH8sFaL-iw';
   const recipeApi = useFetch(
     'http://localhost:3000/recipes/',
@@ -28,31 +21,8 @@ export default function Admin(props) {
   const [time, setTime] = useState();
   const [instructions, setInstructions] = useState();
   const [foodType, setFoodType] = useState();
-  const [count, setCount] = useState([]);
-  const [unit, setUnit] = useState([]);
-  const [ingredient, setIngredient] = useState();
 
   const [ingredients, setIngredients] = useState([null]);
-
-  const handleIngidientSubmit = (e) => {
-    alert(`A name was submitted: ${JSON.stringify(ingredients)}`);
-    e.preventDefault();
-  };
-
-  const addClick = () => {
-    setIngredients(() => ({
-      ingredients: [...ingredients, { count: '', unit: '', ing: '' }],
-    }));
-  };
-
-  const removeClick = (i) => {
-    ingredients.splice(i, 1);
-    setIngredients({ ingredients });
-  };
-
-  const handleChange = (i, e, name) => {
-    setIngredients({ ...ingredients, [name]: e.target.value });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault(e);
@@ -77,28 +47,10 @@ export default function Admin(props) {
       });
   };
 
-  const createUI = () => ingredients.map((el, i) => (
-    <div key={i}>
-      <input
-        placeholder="Antal"
-        name="count"
-        value={el.count || ''}
-        onChange={handleChange(i)}
-      />
-      <input placeholder="Enhet" name="unit" value={el.unit || ''} onChange={handleChange(i)} />
-      <input placeholder="Ingrediens" name="ing" value={el.ing || ''} onChange={handleChange(i)} />
-      <input type="button" value="remove" onClick={removeClick(i)} />
-    </div>
-  ));
-
+  
   return (
     <div className="admin">
       <h1>Admin</h1>
-      <form onSubmit={handleIngidientSubmit}>
-        {createUI()}
-        <input type="button" value="add more" onClick={() => { addClick(); }} />
-        <input type="submit" value="Submit" />
-      </form>
       <div className="admin-addrecept">
         <h2>Nytt Recept</h2>
         <h4>Antal Ingredienser:</h4>
@@ -123,23 +75,7 @@ export default function Admin(props) {
           <label>Kategori Kolhydrat: <input type="text" name="category2" onChange={(e) => { setcategory2(e.target.value); }} /></label>
           <label>Bildlänk:<input type="text" name="imageLink" onChange={(e) => { setImageLink(e.target.value); }} /></label>
           <label>Tid: <input type="text" name="time" onChange={(e) => { setTime(e.target.value); }} /></label>
-
-          {
-            numberOfIngridients ? (
-              [...Array(numberOfIngridients)].map((e, i) => (
-                <div key={i}>
-                  <p>Ingrediens 1:</p>
-                  <label>Antal:<input type="text" name="count" onChange={(e) => { setCount(e.target.value); }} /></label>
-                  <label>Enhet:<input type="text" name="unit" onChange={(e) => { setUnit(e.target.value); }} /></label>
-                  <label>Vara:<input type="text" name="ingidient" onChange={(e) => { setIngredient(e.target.value); }} /></label>
-                </div>
-              ))
-            ) : (
-              <p>välj antal ingredienser:</p>
-            )
-          }
           <hr />
-
           <label>Instruktioner 1:<input type="text" name="instructions" onChange={(e) => { setInstructions(e.target.value); }} /></label>
           <label>Instruktioner 2:<input type="text" name="instructions" /></label>
           <label>Allergy:<input type="text" name="allergy" /></label>
@@ -158,7 +94,6 @@ export default function Admin(props) {
       </div>
       <div className="admin-receptlist">
         <h2>Receptlista</h2>
-        <div />
       </div>
     </div>
   );
