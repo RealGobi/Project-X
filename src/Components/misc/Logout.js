fe9a6c8c1cdc80f3ee10eea7cdde842479a37804
Jimmy Svensson <jimmy888swe@gmail.com>
2019-12-20 00:40:02 +0100

    more use for my BFF in logout

--- src/Components/misc/Logout.js
+++ src/Components/misc/Logout.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logoutMe } from '../../actions/authAction';
 
-const Logout = () => (
-  <button type="button" onClick={logoutMe} href="#">
+const Logout = () => {
+  const history = useHistory();
+  const out = () => {
+    history.push('/');
+  };
+  return (
+    <button type="button" onClick={() => { logoutMe(); out(); }} href="#">
        Logga ut
-  </button>
-);
+    </button>
+  );
+};
 
 
 export default connect(null, { logoutMe })(Logout);
