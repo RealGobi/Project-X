bb1399f885da205f3743cb3e7d994c24564bba50
Jimmy Svensson <jimmy888swe@gmail.com>
2019-12-21 09:37:36 +0100

    getState in app befor render, a hard nut to crack

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ import ReciptPage from './pages/Recipt-page';
 import SearchList from './pages/Search-list';
 import Admin from './pages/Admin';
 
-const App = (recipe) => {
+const App = (getState) => {
+  const { recipes } = getState.recipe;
+  console.log(recipes);
   useEffect(() => {
     const dispatchActions = async () => {
       await store.dispatch(loadUser());
@@ -35,16 +37,16 @@ const App = (recipe) => {
   
   // filter out recipe
   
-  console.log(recipe);
-  const findRecipe = recipe.find(rec => rec._id === chosenRecipe);
-  const findRecipeBasedOnOne = recipe.filter(rec => rec.category1.find(r => r === categoryOne));
+  console.log(recipes);
+  const findRecipe = recipes.find(rec => rec._id === chosenRecipe);
+  const findRecipeBasedOnOne = recipes.filter(rec => rec.category1.find(r => r === categoryOne));
 
   // filter out category
 
   // collect all categorys to one array
   let category1 = [];
   const collectCategory1 = () => {
-    recipe.map(cat => cat.category1.map(tac => category1.push(tac)));
+    recipes.map(cat => cat.category1.map(tac => category1.push(tac)));
   };
   collectCategory1();
   const categorylist1 = category1;
@@ -53,7 +55,7 @@ const App = (recipe) => {
 
   let category2 = [];
   const collectCategory2 = () => {
-    recipe.map(cat => cat.category2.map(tac => category2.push(tac)));
+    recipes.map(cat => cat.category2.map(tac => category2.push(tac)));
   };
   collectCategory2();
   const categorylist2 = category2;
@@ -69,12 +71,12 @@ const App = (recipe) => {
           <Route path="/" exact render={() => <Login />} />
           <Route path="/signup" component={SignUp} />
           <Route path="/landing-page" component={LandingPage} />
-          <Route path="/choose-first" render={() => <ChooseFirst recipe={recipe} setCategoryOne={setCategoryOne} category1={category1} />} />
+          <Route path="/choose-first" render={() => <ChooseFirst recipe={recipes} setCategoryOne={setCategoryOne} category1={category1} />} />
           <Route path="/choose-second" render={() => <ChooseSecond findRecipeBasedOnOne={findRecipeBasedOnOne} setCategoryTwo={setCategoryTwo} />} />
           <Route path="/recipt-list" render={() => <ReciptList findRecipeBasedOnOne={findRecipeBasedOnOne} setChosenRecipe={setChosenRecipe} />} />
           <Route path="/recipt-page" render={() => <ReciptPage findRecipe={findRecipe} />} />
-          <Route path="/search-list" render={() => <SearchList setChosenRecipe={setChosenRecipe} recipe={recipe} category1={category1} category2={category2} />} />
-          <Route path="/admin" render={() => <Admin recipe={recipe} />} />
+          <Route path="/search-list" render={() => <SearchList setChosenRecipe={setChosenRecipe} recipe={recipes} category1={category1} category2={category2} />} />
+          <Route path="/admin" render={() => <Admin recipe={recipes} />} />
         </Switch>
       </div>
     </Router>
